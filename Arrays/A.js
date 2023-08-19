4defe7148f0afb2c345dd00d7f895caf792b2733
Sounak Pramanick <sounakmsd7@gmail.com>
2023-08-20 00:00:35 +0530

    Leetcode 2824

---
+++ Arrays/A.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    let n = nums.length;
+    let i = 0, j = n - 1;
+    let count = 0;
+
+    nums.sort((a, b) => a - b);
+
+    while(i < j) {
+        if(nums[i] + nums[j] >= target) j--;
+        else {
+            count += j - i;
+            i++;
+        }
+    }
+
+    return count;
+};
