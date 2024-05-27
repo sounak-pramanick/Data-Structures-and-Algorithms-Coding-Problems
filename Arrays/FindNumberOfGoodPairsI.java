798343fc6343f59d55d9ae877ec9e020f5b6fa72
Sounak Pramanick <sounakmsd7@gmail.com>
2024-05-27 21:17:17 +0530

    Leetcode 3162

---
+++ Arrays/FindNumberOfGoodPairsI.java
@@ -0,0 +1,14 @@
+// Leetcode 3162
+
+class Solution {
+    public int numberOfPairs(int[] nums1, int[] nums2, int k) {
+        int goodPairs = 0;
+        for(int i = 0; i < nums1.length; i++) {
+            for(int j = 0; j < nums2.length; j++) {
+                if((nums1[i] / (double)nums2[j]) % k == 0) goodPairs++;
+            }
+        }
+
+        return goodPairs;
+    }
+}
